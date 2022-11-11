db3e345210f4d00b42e538af4c6dcf1730f25205
romanpajak <romanpajak@interia.pl>
2022-11-11 17:47:55 +0100

    some changes

--- index.js
+++ index.js
@@ -40,48 +40,88 @@ const solutions = {
     field7: "",
   },
 };
-  const fieldArea = document.getElementsByClassName("field");
-function addClicks(){
+
+const fieldArea = document.getElementsByClassName("field");
+
+function addClicks() {
   for (let y = 0; y < fieldArea.length; y++) {
     fieldArea[y].addEventListener("click", clickField);
   }
+}
+
+function resetGame() {
+  for (let i = 1; i < 10; i++) {
+    const contentField = document.getElementById(`field${i}`);
+    contentField.innerHTML = "";
+    contentField.className = "field";
   }
-function resetGame(){
-  for (let i=1;i<10;i++){
-    const contentField=document.getElementById(`field${i}`);
-   contentField.innerHTML="";
-   contentField.className="field"
-  }
-  for (x in solutions){
+  for (let x in solutions) {
     let keysArr = Object.keys(solutions[x]);
-    for (i = 0; i < keysArr.length; i++) {
-        solutions[x][keysArr[i]] = "";
+    for (let i = 0; i < keysArr.length; i++) {
+      solutions[x][keysArr[i]] = "";
     }
   }
   addClicks();
-  document.querySelector(".infoStatus").innerHTML="";
+  document.querySelector(".infoStatus").innerHTML = "";
   countInStatistics("matches-details");
 }
 
+function ifWinningPossible() {
+  for (let x in solutions) {
+    let valuesArr = Object.values(solutions[x]);
+    let keysArr = Object.keys(solutions[x]);
+    if (valuesArr.indexOf("x") < 0) {
+      let markCounter = 0;
+      for (let i = 0; i < valuesArr.length; i++) {
+        if (valuesArr[i] === "o") {
+          markCounter++;
+        }
+      }
+      if (markCounter === 2) {
+        for (let i = 0; i < valuesArr.length; i++) {
+          if (valuesArr[i] === "") {
+            markField(keysArr[i], "circle");
+            document
+              .getElementById(keysArr[i])
+              .removeEventListener("click", clickField);
+            markInObject(keysArr[i], "o");
 
-function markField(){
-  const fld=document.getElementById("field1");
-  const cross="<span class='cross'>&#9587</span>";
-  fld.innerHTML=cross;
-}
-function ifMiddleFieldEmpty() {
-  for (x in solutions) {
-    //let keysArr=Object.keys(solutions[x]);
-    if (solutions[x]["field5"] === "") {
-      return true;
-      break;
+            for (let y = 0; y < keysArr.length; y++) {
+              document
+                .getElementById(keysArr[y])
+                .classList.add("field-highlight-if-loose");
+            }
+
+            showInformation("Niestety przegrałeś.");
+            countInStatistics("fails-details");
+            clearClicks();
+            return true;
+            break;
+          }
+        }
+      }
     }
   }
-  return false;
+}
+
+function markField(fieldName, sign) {
+  const fld = document.getElementById(fieldName);
+  if (sign === "cross") {
+    const cross = "<span class='cross'>&#9587</span>";
+    setTimeout(() => {
+      fld.innerHTML = cross;
+    }, 50);
+  }
+  if (sign === "circle") {
+    const circle = "<span class='circle'>&#9711</span>";
+    setTimeout(() => {
+      fld.innerHTML = circle;
+    }, 100);
+  }
 }
 
 function markInObject(field, mark) {
-  for (x in solutions) {
+  for (let x in solutions) {
     let keysArr = Object.keys(solutions[x]);
     for (i = 0; i < keysArr.length; i++) {
       if (keysArr[i] === field) {
@@ -90,218 +130,150 @@ function markInObject(field, mark) {
     }
   }
 }
-function ifBlockingPossible() {
-  for (x in solutions) {
+
+function ifComputerLose() {
+  for (let x in solutions) {
     let valuesArr = Object.values(solutions[x]);
-    keysArr = Object.keys(solutions[x]);
+    let keysArr = Object.keys(solutions[x]);
     if (valuesArr.indexOf("o") < 0) {
       let markCounter = 0;
-      for (i = 0; i < valuesArr.length; i++) {
+      for (let i = 0; i < valuesArr.length; i++) {
+        if (valuesArr[i] === "x") {
+          markCounter++;
+        }
+      }
+      if (markCounter === 3) {
+        for (y = 0; y < keysArr.length; y++) {
+          document
+            .getElementById(keysArr[y])
+            .classList.add("field-highlight-if-win");
+        }
+
+        showInformation("Brawo! Wygrałeś.");
+        countInStatistics("win-details");
+        clearClicks();
+        return true;
+        break;
+      }
+    }
+  }
+}
+
+function showInformation(text) {
+  document.querySelector(".infoStatus").innerHTML = text;
+}
+
+function countInStatistics(class_name) {
+  const startingValue = document.body
+    .getElementsByClassName(class_name)[0]
+    .getElementsByTagName("p")[0].innerHTML;
+  document
+    .getElementsByClassName(class_name)[0]
+    .getElementsByTagName("p")[0].innerHTML = parseFloat(startingValue) + 1;
+}
+
+function ifMiddleFieldEmpty() {
+  for (let x in solutions) {
+    if (solutions[x]["field5"] === "") {
+      markInObject("field5", "o");
+      markField("field5", "circle");
+      document
+        .getElementById("field5")
+        .removeEventListener("click", clickField);
+      return true;
+    }
+  }
+}
+
+function ifBlockingPossible() {
+  for (let x in solutions) {
+    let valuesArr = Object.values(solutions[x]);
+    let keysArr = Object.keys(solutions[x]);
+    if (valuesArr.indexOf("o") < 0) {
+      let markCounter = 0;
+      for (let i = 0; i < valuesArr.length; i++) {
         if (valuesArr[i] === "x") {
           markCounter++;
         }
       }
       if (markCounter === 2) {
-        return true;
-        break;
-      }
-    }
-  }
-  return false;
-}
-
-function ifWinningPossible() {
-  for (x in solutions) {
-    let valuesArr = Object.values(solutions[x]);
-    keysArr = Object.keys(solutions[x]);
-    if (valuesArr.indexOf("x") < 0) {
-      let markCounter = 0;
-      for (i = 0; i < valuesArr.length; i++) {
-        if (valuesArr[i] === "o") {
-          markCounter++;
-        }
-      }
-      if (markCounter === 2) {
-        return true;
-        break;
-      }
-    }
-  }
-}
-
-function ifComputerLose(){
-    for (x in solutions) {
-        let valuesArr = Object.values(solutions[x]);
-        keysArr = Object.keys(solutions[x]);
-        if (valuesArr.indexOf("o") < 0) {
-          let markCounter = 0;
-          for (i = 0; i < valuesArr.length; i++) {
-            if (valuesArr[i] === "x") {
-              markCounter++;
-            }
-          }
-          if (markCounter === 3) {
-            for(y=0;y<keysArr.length;y++){
-              document.getElementById(keysArr[y]).classList.add("field-highlight-if-win")
-            }
-
+        for (let i = 0; i < valuesArr.length; i++) {
+          if (valuesArr[i] === "") {
+            markField(keysArr[i], "circle");
+            document
+              .getElementById(keysArr[i])
+              .removeEventListener("click", clickField);
+            markInObject(keysArr[i], "o");
             return true;
             break;
           }
         }
       }
-}
-function showInformation(text){
-    document.querySelector(".infoStatus").innerHTML=text;
-}
-function countInStatistics(class_name){
-const startingValue=document.body.getElementsByClassName(class_name)[0].getElementsByTagName("p")[0].innerHTML;
-document.getElementsByClassName(class_name)[0].getElementsByTagName("p")[0].innerHTML= parseFloat(startingValue)+1;
-}
-//zrobić pętlę
-function clearClicks(){
-    for (let y = 0; y < fieldArea.length; y++) {
-        fieldArea[y].removeEventListener("click", clickField);
-      }  
-}
-addClicks();
-function highlightFields(cross,circle){
-
-}
-// document.getElementsByClassName("field")[0].addEventListener("click",clickField);
-//everyField
-
-function clickField() {
-//   this.innerHTML = "x";
-  fieldKey = this.id;
-  document.getElementById(fieldKey).innerHTML="x"
-  this.removeEventListener("click", clickField);
-  markInObject(fieldKey, "x");
-  if (ifComputerLose()===true){
-    showInformation("Brawo! Wygrałeś.");
-    countInStatistics("win-details");
-    clearClicks();
-    return;
+    }
   }
+}
 
-  if (ifWinningPossible() === true) {
-    //alert("możliwe zwycięstwo komputera ");
-   
-    for (x in solutions) {
-      let valuesArr = Object.values(solutions[x]);
-      keysArr = Object.keys(solutions[x]);
-      if (valuesArr.indexOf("x") < 0) {
-        let markCounter = 0;
-        for (i = 0; i < valuesArr.length; i++) {
-          if (valuesArr[i] === "o") {
-            markCounter++;
-          }
-        }
-        if (markCounter === 2) {
-          for (i = 0; i < valuesArr.length; i++) {
-            if (valuesArr[i] === "") {
-              //solutions[x][i]="o"
-              //solutions[x][keysArr[i]]="o";
-              document.getElementById(keysArr[i]).innerHTML = "o";
-              document
-                .getElementById(keysArr[i])
-                .removeEventListener("click", clickField);
-              markInObject(keysArr[i], "o");
-              
-              for(y=0;y<keysArr.length;y++){
-                document.getElementById(keysArr[y]).classList.add("field-highlight-if-loose")
-              }
-              
-              showInformation("Niestety przegrałeś.");
-              countInStatistics("fails-details");
-              clearClicks();
-              break;
-              
-              
-              
-              return;
-
-
-
-            }
-          }
+function randomMove() {
+  let fieldsArr = [];
+  for (let x in solutions) {
+    keysArr = Object.keys(solutions[x]);
+    for (let i = 0; i < keysArr.length; i++) {
+      if (solutions[x][keysArr[i]] === "") {
+        if (fieldsArr.indexOf(keysArr[i]) < 0) {
+          fieldsArr.push(keysArr[i]);
         }
       }
     }
-
   }
 
-  if (ifBlockingPossible() === true) {
-    //alert("możliwe zablokowanie");
- for (x in solutions) {
-      let valuesArr = Object.values(solutions[x]);
-      keysArr = Object.keys(solutions[x]);
-      if (valuesArr.indexOf("o") < 0) {
-        let markCounter = 0;
-        for (i = 0; i < valuesArr.length; i++) {
-          if (valuesArr[i] === "x") {
-            markCounter++;
-          }
-        }
-        if (markCounter === 2) {
-          for (i = 0; i < valuesArr.length; i++) {
-            if (valuesArr[i] === "") {
-              //solutions[x][i]="o"
-              //solutions[x][keysArr[i]]="o";
-              document.getElementById(keysArr[i]).innerHTML = "o";
-              document
-                .getElementById(keysArr[i])
-                .removeEventListener("click", clickField);
-              markInObject(keysArr[i], "o");
-              break;
-            }
-          }
-          return;
-        }
-      }
-    }
+  const counter = fieldsArr.length;
 
-
-
-  }
-
-
-
-  if (ifBlockingPossible() === false && ifMiddleFieldEmpty() === true) {
-    //alert("możliwe zajęcie środkowego pola");
-
-    markInObject("field5", "o");
-    document.getElementById("field5").innerHTML = "o";
-    document.getElementById("field5").removeEventListener("click", clickField);
-  } else if (ifBlockingPossible() === false && ifMiddleFieldEmpty() === false) {
-    const fieldsArr = [];
-    for (x in solutions) {
-      keysArr = Object.keys(solutions[x]);
-      for (i = 0; i < keysArr.length; i++) {
-        if (solutions[x][keysArr[i]] === "") {
-          if (fieldsArr.indexOf(keysArr[i]) < 0) {
-            fieldsArr.push(keysArr[i]);
-          }
-        }
-      }
-    }
-
-    const counter = fieldsArr.length;
-    if(counter>1){
+  if (counter > 1) {
     const randomNumb = Math.floor(Math.random() * counter);
     const fieldToRandomMark = fieldsArr[randomNumb];
-    //alert(fieldToRandomMark);
     markInObject(fieldToRandomMark, "o");
-    document.getElementById(fieldToRandomMark).innerHTML = "o";
+    markField(fieldToRandomMark, "circle");
     document
       .getElementById(fieldToRandomMark)
       .removeEventListener("click", clickField);
-
-    return;
-  }else{
-    showInformation("Remis")
-    countInStatistics("draws-details")
+  }
+  if (counter === 0) {
+    showInformation("Remis");
+    countInStatistics("draws-details");
     return;
   }
 }
+
+function clearClicks() {
+  for (let y = 0; y < fieldArea.length; y++) {
+    fieldArea[y].removeEventListener("click", clickField);
+  }
+}
+
+addClicks();
+
+function highlightFields(cross, circle) {}
+
+function clickField() {
+  const fieldKey = this.id;
+  markInObject(fieldKey, "x");
+  markField(fieldKey, "cross");
+  this.removeEventListener("click", clickField);
+  if (ifComputerLose === true) {
+    return;
+  }
+  if (ifComputerLose() === true) {
+    return;
+  }
+  if (ifWinningPossible() === true) {
+    return;
+  }
+
+  if (ifBlockingPossible() === true) {
+    return;
+  }
+  if (ifMiddleFieldEmpty() === true) {
+    return;
+  }
+  randomMove();
 }
